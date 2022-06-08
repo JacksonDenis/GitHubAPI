63dbbac5fceb00ca90802cccc1d11ee929418aec
Jackson Denis <jackson.denis@live.com>
2022-06-08 17:12:31 -0300

    Header parte 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import Layout from './componets/layout';
 
 
 function App() {
-  return <div className="App">GitHub</div>;
+  return (
+    <main>
+      <Layout>
+        <div>
+          <img src="https://avatars.githubusercontent.com/u/1?v=4" alt="Foto do Usuario" />
+          <h1>Jackson Denis</h1>
+          <h3>Username</h3>
+          <span>Benits</span>
+          <div>
+            <h4>Followers</h4>
+            <span>5</span>
+          </div>
+          <div>
+            <h4>Starreds</h4>
+            <span>5</span>
+          </div>
+          <div>
+            <h4>Followings</h4>
+            <span>5</span>
+          </div>
+        </div>
+        <div>Reporitories</div>
+        <div>Starreds</div>
+      </Layout>
+    </main>
+  )
 }
 
 export default App;
